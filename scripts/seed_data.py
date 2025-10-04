67c3e7ef7b73da504231be363098f534ee849794
germanbursetromero <bursetromerogerman@gmail.com>
2025-10-04 15:46:46 -0400

    few changes made to model and seed regarding emails

--- scripts/seed_data.py
+++ scripts/seed_data.py
@@ -22,7 +22,7 @@ mentor4 = models.User(email="billy@rowan.edu", name="Billy", role="mentor")
 student1 = models.User(email="charlie@rvcc.edu", name="Charlie", role="student")
 student2 = models.User(email="jackson@uccc", name="Jackson", role="student")
 student3 = models.User(email="mark@rvcc.edu", name="Mark", role="student")
-student4 = models.user(email="john@mccc", name="John", role="student")
+student4 = models.User(email="john@mccc", name="John", role="student")
 
 db.add_all([mentor1, mentor2, mentor3, mentor4, student1, student2, student3, student4])
 db.commit()
@@ -31,47 +31,39 @@ db.commit()
 m1_profile = models.Mentor(user_id=mentor1.id,
                            university="Rutgers",
                            area_of_study="technology",
-                           bio="Transfered from RVCC into Rutgers, happy to help with anything!",
-                           mentor_email=mentor1.email)
+                           bio="Transfered from RVCC into Rutgers, happy to help with anything!")
 
 m2_profile = models.Mentor(user_id=mentor2.id,
                            university="Rutgers",
                            area_of_study="engineering",
-                           bio="Transfered from RVCC into Princeton, I'll help you achieve your goals!",
-                           mentor_email=mentor2.email)
+                           bio="Transfered from RVCC into Princeton, I'll help you achieve your goals!")
 
 m3_profile = models.Mentor(user_id=mentor3.id,
                            university="Rutgers",
                            area_of_study="liberal arts",
-                           bio="Transfered from UCC into Rutgers, happy to assist!",
-                           mentor_email=mentor3.email)
+                           bio="Transfered from UCC into Rutgers, happy to assist!")
 
 m4_profile = models.Mentor(user_id=mentor4.id,
                            university="Rutgers",
                            area_of_study="engineering",
-                           bio="Transfered from MCCC into Rowan University, I'll help you out!",
-                           mentor_email=mentor4.email)
+                           bio="Transfered from MCCC into Rowan University, I'll help you out!")
 
 # create student profile
 s1_profile = models.Student(user_id=student1.id,
                             college="RVCC",
-                            intended_area_of_study="liberal arts",
-                            student_email=student1.email)
+                            intended_area_of_study="liberal arts")
 
 s2_profile = models.Student(user_id=student2.id,
                             college="UCCC",
-                            intended_area_of_study="engineering",
-                            student_email=student2.email)
+                            intended_area_of_study="engineering")
 
 s3_profile = models.Student(user_id=student3.id,
                             college="RVCC",
-                            intended_area_of_study="engineering",
-                            student_email=student3.email)
+                            intended_area_of_study="engineering")
 
 s4_profile = models.Student(user_id=student4.id,
                             college="MCCC",
-                            intended_area_of_study="technology",
-                            student_email=student4.email)
+                            intended_area_of_study="technology")
 
 db.add_all([m2_profile, m2_profile, m3_profile, m4_profile, s1_profile, s2_profile, s3_profile, s4_profile])
 db.commit()
